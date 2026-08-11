011696b13f008b715f8e398e17ba2795564958f0
GANESHVEERABOINA <ganeshveeraboina.pro@gmail.com>
2026-08-11 15:04:03 +0530

    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Change 'ganesh-portfolio' to your actual GitHub repository name
-  base: '/ganesh-portfolio/', 
+  // ఇది చాలా ముఖ్యం: మీ GitHub Repository పేరు ఇక్కడ కరెక్ట్ గా ఉండాలి
+  base: '/Ganesh-Portfolio-website/', 
 })
