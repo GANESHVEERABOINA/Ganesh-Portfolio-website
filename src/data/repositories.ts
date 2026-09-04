279f5cba9e241963adfcfd6d0ee23e7dbe422619
GANESH VEERABOINA <ganeshveeraboina.pro@gmail.com>
2026-09-04 16:51:49 +0530

    Update GitHub URLs and technologies in repositories.ts
    Updated GitHub repository links and added Ansible to the technologies list.

--- src/data/repositories.ts
+++ src/data/repositories.ts
@@ -3,12 +3,12 @@ export const repositoriesData = [
     title: "DevOps-CI-CD-Pipeline",
     description: "Automated build and deployment workflows using Jenkins pipelines integrated with GitHub and Dockerized applications on AWS EC2.",
     technologies: ["Jenkins", "Docker", "AWS", "Groovy", "Maven"],
-    githubUrl: "#" // Replace with your actual GitHub repo link
+    githubUrl: "https://github.com/GANESHVEERABOINA/devops-cicd-project.git" // Replace with your actual GitHub repo link
   },
   {
     title: "AWS-Infrastructure-Automation",
     description: "Repository containing infrastructure configurations and deployment scripts for AWS services.",
-    technologies: ["AWS", "Terraform", "Linux", "Bash"],
-    githubUrl: "#" // Replace with your actual GitHub repo link
+    technologies: ["AWS", "Terraform", "Linux", "Bash", "Ansible"],
+    githubUrl: "https://github.com/GANESHVEERABOINA/AWS-Infrastructure-Automation.git" // Replace with your actual GitHub repo link
   }
 ];
