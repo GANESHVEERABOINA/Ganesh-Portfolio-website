a564baaaf92460d6a4116fe0d036a940ae14a5a8
GANESH VEERABOINA <ganeshveeraboina.pro@gmail.com>
2026-09-05 12:35:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ఇది చాలా ముఖ్యం: మీ GitHub Repository పేరు ఇక్కడ కరెక్ట్ గా ఉండాలి
-  base: '/Ganesh-Portfolio-website/', 
+  // Github Pages ki mariyu Docker Nginx ki rendu chotla work ayye single relative path!
+  base: './', 
 })
-// fresh commit to clear artifacts
